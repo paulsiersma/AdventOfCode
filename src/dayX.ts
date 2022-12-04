8c91d2058f6b0d75085c23996148455057f16bad
Paul Siersma <paul@liquidlogic.eu>
2022-12-04 14:39:35 +0100

    Day4 plumbing

---
+++ src/dayX.ts
@@ -0,0 +1,16 @@
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+export function solveX_1(inputPath: string): number {
+    return 0;
+}
+
+export function solveX_2(inputPath: string): number {
+    return 0;
+}
+
+var parseInput = (inputPath: string): string[][] => {
+    const file = readFileSync(inputPath, 'utf-8');
+
+    return [];
+}
--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,27 @@ import * as path from 'path';
 import { expect } from 'earljs'
 
 import { solve1_1, solve1_2 } from "./day1";
-testAndSolve("Day 1", solve1_1, solve1_2, "day1_test1", "day1_1", 24000, 45000)
+testAndSolve("Day 1 Part 1", solve1_1, "day1_test1", "day1_1", 24000)
+testAndSolve("Day 1 Part 2", solve1_2, "day1_test1", "day1_1", 45000)
 
 import { solve2_1, solve2_2 } from "./day2";
-testAndSolve("Day 2", solve2_1, solve2_2, "day2_test1", "day2_1", 15, 12)
+testAndSolve("Day 2 Part 1", solve2_1, "day2_test1", "day2_1", 15)
+testAndSolve("Day 2 Part 2", solve2_2, "day2_test1", "day2_1", 12)
 
 import { solve3_1, solve3_2 } from "./day3";
-testAndSolve("Day 3", solve3_1, solve3_2, "day3_test1", "day3_1", 157, 70)
+testAndSolve("Day 3 Part 1", solve3_1, "day3_test1", "day3_1", 157)
+testAndSolve("Day 3 Part 2", solve3_2, "day3_test1", "day3_1", 70)
+
+import { solve4_1, solve4_2 } from "./day4";
+testAndSolve("Day 4 Part 1", solve4_1, "day4_test1", "day4_1", 2)
+testAndSolve("Day 4 Part 2", solve4_2, "day4_test1", "day4_1", 4)
 
 
 
 
-
-function testAndSolve(id: string, solve1: (string) => number, solve2: (string) => number, testInput1Id: string, input1Id: string, solve1TestInputAnswer: number, solve2TestInputAnswer: number) {
-    expect(solve1(getInputPath(testInput1Id))).toEqual(solve1TestInputAnswer);
-    console.log(`${id} Part 1`, solve1(getInputPath(input1Id)));
-    expect(solve2(getInputPath(testInput1Id))).toEqual(solve2TestInputAnswer);
-    console.log(`${id} Part 2`, solve2(getInputPath(input1Id)));
+function testAndSolve(id: string, solve: (string) => number, testInputId: string, inputId: string, solveTestInputAnswer: number) {
+    expect(solve(getInputPath(testInputId))).toEqual(solveTestInputAnswer);
+    console.log(`${id}`, solve(getInputPath(inputId)));
 }
 
 function getInputPath(id: string) {
